09c0ddb38c8957340df9059389682c87a5da8eef
Landon Shields <landonwshields@gmail.com>
2018-02-14 17:15:48 -0700

    added projects data

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -8,6 +8,50 @@ export default class Projects extends Component {
         <h2>
           Projects:
         </h2>
+        <h4>
+          Superior Learning Academy - Capstone
+        </h4>
+          <a href='https://github.com/landonwshields/sla'>
+            Github
+          </a>
+          <p>
+            The easiest to update website available, even my mom can use it!
+            <br />
+            JavaScript, React, CSS, PostgresQL, Express, Node.js, knex.js, Heroku
+          </p>
+        <h4>
+          Roofstops
+        </h4>
+          <a href='https://github.com/landonwshields/Roofstops'>
+            Github
+          </a>
+          <p>
+            Roofstops is a new app to find rooftop bars and patios nearest you!
+            <br />
+            JavaScript, React Native, HTML, CSS
+          </p>
+        <h4>
+          CLIQUE
+        </h4>
+          <a href='https://github.com/landonwshields/CLIQUE'>
+            Github
+          </a>
+          <p>
+            A group sourced networking platform.
+            <br />
+            HTML, CSS, JavaScript, Postgresql, Express, Node.js, Handlebars.js, Knex.js, Heroku
+          </p>
+        <h4>
+          Drug Dominate
+        </h4>
+          <a href='https://github.com/shieldsl/Q1-project-DrugDominate'>
+            Github
+          </a>
+          <p>
+            An app that allows input of any two medications and it will inform the user of any interactions they may have.
+            <br />
+            HTML, CSS, JavaScript, Bootstrap, Firebase
+          </p>
       </div>
     );
   }
